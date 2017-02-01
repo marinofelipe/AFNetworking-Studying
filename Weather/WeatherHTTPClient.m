a94b7b2bd0163d2eb09407f105a4118c0f98ef4d
Felipe Marino <felipemarino91@gmail.com>
2017-02-01 15:58:29 -0200

    Created Weather client class to make calls to real weather API

---
+++ Weather/WeatherHTTPClient.m
@@ -0,0 +1,18 @@
+//
+//  WeatherHTTPClient.m
+//  Weather
+//
+//  Created by Felipe Marino on 2/1/17.
+//  Copyright © 2017 Scott Sherwood. All rights reserved.
+//
+
+#import "WeatherHTTPClient.h"
+
+static NSString * const kWorldWeatherOnlineAPIKey = @"b6e116eed6bb4cdf822134901170102";
+static NSString * const kWorldWeatherOnlineURLString = @"http://api.worldweatheronline.com/free/v1/";
+
+@implementation WeatherHTTPClient
+
+
+
+@end
